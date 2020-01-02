506bb25f7118e7a64c8a7ff5fd18293f7de2645f
Kabuto <591775790@qq.com>
2020-01-02 18:41:00 +0800

    React-练习实例-ref，生命周期函数

--- React/demo01/src/XiaoJieJie.js
+++ React/demo01/src/XiaoJieJie.js
@@ -2,7 +2,9 @@
  * 组件
  */
 import React, { Component, Fragment } from 'react'
+import axios from 'axios'
 import XiaoJieJieItem from './XiaoJieJieItem'
+import Boss from './Boss'
 import './xiaojiejie.css'
 
 // Fragment 可以替代最外层的div
@@ -17,13 +19,47 @@ class XiaoJieJie extends Component {
     this.inputChange = this.inputChange.bind(this)
     this.deleteItem = this.deleteItem.bind(this)
   }
+  /**
+   * setState是一个异步函数，如果需要在更新数据之后进行操作，
+   * 可以使用setState的第二个参数回调函数（类似Vue.nextTick())
+   */
+  inputChange (e) {
+    // this.state.inputValue = e.target.value // 无效
+    this.setState({
+      inputValue: this.input.value
+    })
+  }
+  addItem () {
+    this.setState({
+      inputValue: '',
+      list: [...this.state.list, this.state.inputValue]
+    }, () => {
+      console.log(this.ul.querySelectorAll('li').length)
+    })
+  }
+  deleteItem (index) {
+    let list = this.state.list
+    list.splice(index, 1)
+    this.setState({
+      list: list
+    })
+  }
+  /**
+   * mounting
+   */
   render () {
+    // console.log('render——————挂载中')
     return (
       <Fragment>
         {/* JSX中的注释方式 */}
-        <input className='input' value={this.state.inputValue} onChange={this.inputChange}></input>
+        <input
+            className='input'
+            value={this.state.inputValue}
+            onChange={this.inputChange}
+            ref={input => {this.input = input}} // 绑定DOM
+        ></input>
         <button onClick={this.addItem}>增加服务</button>
-        <ul>
+        <ul ref={ul => {this.ul = ul}}>
           {
             this.state.list.map((item, index) => {
               // return ()括号里面可以换行
@@ -43,28 +79,33 @@ class XiaoJieJie extends Component {
             })
           }
         </ul>
+        <Boss></Boss>
       </Fragment>
     )
   }
-  inputChange (e) {
-    // this.state.inputValue = e.target.value // 无效
-    this.setState({
-      inputValue: e.target.value
-    })
+  // componentWillMount () {
+  //   console.log('componentWillMount——————将要挂载')
+  // }
+  componentDidMount () {
+    console.log('componentDidMount——————挂载完成')
+    axios.post('https://web-api.juejin.im/v3/web/wbbr/bgeda')
+        .then(res => { console.log('axios获取成功' + JSON.stringify(res)) })
+        .catch(err => { console.log('axios获取失败' + err)})
   }
-  addItem () {
-    this.setState({
-      inputValue: '',
-      list: [...this.state.list, this.state.inputValue]
-    })
-  }
-  deleteItem (index) {
-    let list = this.state.list
-    list.splice(index, 1)
-    this.setState({
-      list: list
-    })
-  }
+  /**
+   * updation---state
+   */
+  // shouldComponentUpdate () {
+  //   console.log('shouldComponentUpdate——————组件更新前1')
+  //   return true // 要返回一个布尔值
+  // }
+  // componentWillUpdate () {
+  //   console.log('componentWillUpdate——————组件更新前2')
+  //   return true
+  // }
+  // componentDidUpdate () {
+  //   console.log('componentDidUpdate——————组件更新后(render之后)')
+  // }
 }
 
 export default XiaoJieJie
--- React/demo01/src/XiaoJieJieItem.js
+++ React/demo01/src/XiaoJieJieItem.js
@@ -1,19 +1,50 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 
 class XiaoJieJieItem extends Component {
   constructor (props) {
     super(props)
     this.handleClick = this.handleClick.bind(this) // 把在JSX中的绑定操作转移到constructor中
   }
-  render() { 
+  render() {
     return (
-      <li onClick={this.handleClick}>{this.props.content}</li>
+      <li onClick={this.handleClick}>
+        {this.props.avname}-{this.props.content}
+      </li>
     )
   }
   handleClick () {
     console.log(this.props.index)
     this.props.deleteItem(this.props.index)
   }
+  shouldComponentUpdate (nextProps, nextState) {
+    return nextProps.content === this.props.content ? true : false
+  }
+  /**
+   * updation---props
+   */
+  // componentWillReceiveProps () {
+  //   // 首次加载不执行，再次加载被传入pros才执行
+  //   console.log('child——————componentWillReceiveProps')
+  // }
+  /**
+   * unmouting
+   */
+  componentWillUnmount () {
+    console.log('child——————componentWillUnmount——————组件被删除之前')
+  }
+}
+
+// 注意propTypes和PropTypes
+XiaoJieJieItem.propTypes = {
+  avname: PropTypes.string.isRequired, // 不传值会报错
+  content: PropTypes.string, // 传值的类型
+  deleteItem: PropTypes.func,
+  index: PropTypes.number
+}
+// 设置默认值
+XiaoJieJieItem.defaultProps = {
+  avname: '桃井老师'
 }
 
 export default XiaoJieJieItem;
